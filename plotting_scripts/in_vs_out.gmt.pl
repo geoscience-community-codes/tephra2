add7c0879e16239de0e543b986a5674d76c3adbb
ljc <lconnor@usf.edu>
2016-04-19 16:20:35 -0400

    Updated plotting routine to GMT version 5.

--- plotting_scripts/in_vs_out.gmt.pl
+++ plotting_scripts/in_vs_out.gmt.pl
@@ -1,11 +1,9 @@
 $points_in = $ARGV[0];
 $points_out = "tephra.out";
-#$points_out = $ARGV[1];
 $points = "plot.data";
 $pointsxy = "plotxy.data";
 $diff_less = "diff.less";
 $diff_more = "diff.more";
-#$out = "in_vs_out.eps";
 $out = $ARGV[1];
 $min=1e12;
 $max=0;
@@ -25,6 +23,7 @@ open (DM, ">$diff_more") || die ("cannot open $diff_more: $!");
 open (DL, ">$diff_less") || die ("cannot open $diff_less: $!");
 $line2 = <DATA_OUT>;
 while (<DATA_IN>) {
+  if ($_ =~ m/^#/) {next;}
   $line1 = $_;
   $line2 = <DATA_OUT>;
   ($e1, $n1, $el1, $data_in[$ct]) = split(" ", $line1);
@@ -33,23 +32,9 @@ while (<DATA_IN>) {
   if ($data_in[$ct] > $max) {$max = $data_in[$ct];}
   if ($data_out[$ct] < $min) {$min = $data_out[$ct];}
   if ($data_out[$ct] > $max) {$max = $data_out[$ct];}
-  $s_data_in = sqrt($data_in[$ct]);
-  $s_data_out = sqrt($data_out[$ct]);
-  printf DATA "%.3f %.3f 0\n", $s_data_in, $s_data_out;
-
-#$data_in /= 10;
-#$data_out /= 10;
-#if ($data_out < $min) {$min = $data_out;}
-# if ($data_out > $max) {$max = $data_out;}
-# $diff = sqrt(($data_in-$data_out)*($data_in-$data_out));
-#if ($data_in > 0) {$diff /= $data_in;}
-
-#system "psxy -JX -R -N -Sc.15i -G0 -V -O -K <<eof>> $out
-#$s_data_in $s_data_out
-#eof
-#";
-
-  
+  $s_data_in = $data_in[$ct];
+  $s_data_out = $data_out[$ct];
+  printf DATA "%.3f %.3f 0\n", $s_data_in, $s_data_out; 
 
   # DOes the model over or under estimate the observed data
 
@@ -73,15 +58,15 @@ while (<DATA_IN>) {
   $ct++;
 }
 
-$pmin = sqrt($min);
-$pmax = sqrt($max);
+$pmin = $min;
+$pmax = $max;
 printf stderr "Min=$pmin  MAX=$pmax\n";
-system "psbasemap --HEADER_FONT_SIZE=14 -JX6i -R$pmin/$pmax/$pmin/$pmax -Ba2g2:'sqrt(Observed [kg m\@+-2\@+])':/a2g2:'sqrt(Calculated [kg m\@+-2\@+)]':/:.'':WS -K -P > $out ";
-system "psxy -JX -R -W2p/255/0/0 -O -K <<eof>> $out
+`gmt psbasemap --FONT_TITLE=12 --FONT_ANNOT_PRIMARY=9p,1,0 --FONT_LABEL=9p,1,0 -JX5i -R$pmin/$pmax/$pmin/$pmax -Bxa50g10+l'Observed [kg m\@+-2\@+]' -Bya50g10+l'Calculated [kg m\@+-2\@+]' -BWS -K -P > $out`;
+`gmt psxy -JX -R -W2p,255/0/0 -O -K <<eof>> $out
 $pmin $pmin
 $pmax $pmax
 eof
-";
+`;
 
 foreach my $dat (@data_in) {
     $log_diff = 0;
@@ -95,16 +80,16 @@ foreach my $dat (@data_in) {
 }
 
 #The normalized root mean squared error (NRMSE) is the RMSE divided by the range of observed values
-system "minmax $points";
+`gmt gmtinfo $points`;
 $rmse /= $ct;
 $rmse = sqrt($rmse);
 $rmse /= ($max - $min);
 $title = sprintf "NRMSE = %.2f", $rmse;
 printf "$title\n";
-system "psbasemap --HEADER_FONT_SIZE=14 -JX -R -B:'':/:'':/:.'$title':wnes -K -O >> $out ";
-system "psxy $points -JX -N -Sc.18i -G0 -R -O >> $out";
+`gmt psbasemap -JX -R -Bwesn+t'$title' -K -O >> $out `;
+`gmt psxy $points -JX -N -Sc.18i -G0 -R -O >> $out`;
 
-system "ps2raster $out -A -Tg";
+`gmt ps2raster $out -A -Tg`;
 system "echo $rmse > nrmse";
 #system "perl write_conf.pl parameters.README tephra2.conf 400 400 $rmse";
 
