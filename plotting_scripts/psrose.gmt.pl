702abfc29c6cfa7707b0fbbe3aca3179cbcb5cf3
ljc <lconnor@usf.edu>
2016-04-19 16:22:14 -0400

    Updated plotting routine to gmt version 5.

--- plotting_scripts/psrose.gmt.pl
+++ plotting_scripts/psrose.gmt.pl
@@ -11,15 +11,16 @@ $max_speed = 0;
 $max_dir = 0;
 $min_dir = 361;
 open (DATA, "<$ARGV[0]") || die ("can't open : $!");
-
+$total = 0;
+$ct = 0;
 while (<DATA>) {
-#foreach $data  (@Data) {
   ($ht, $sp, $dir) = split " ", $_;
-  #print "$data\n";
+
   if ($ht =~ m/^#/) {}
   else {
   	
   	$ht /= 1000;
+  	
   	#if ($dir >= 180) {$dir -=360;}
     if ($ht >= $max_ht) { $max_ht = int($ht) +1; }
   	if ($ht <= $min_ht) { $min_ht = int($ht); }
@@ -27,19 +28,34 @@ while (<DATA>) {
   	if ($sp <= $min_speed) { $min_speed = int($sp); }
   	if ($dir >= $max_dir) { $max_dir = int($dir); }
   	if ($dir <= $min_dir) { $min_dir = int($dir); }
-  	$sp *= .2;
-    printf IN "%.0f 0 %f %f %f\n", $dir, $ht, $dir, $sp;
+  	
+  	$sp *= .16;  
+  	$total += $sp;	
+    $height[$ct] = $ht;
+    $speed[$ct] = $sp;
+    $degree[$ct] = $dir;
+    $ct++;
   }
 }
-printf STDERR "Height Speed Direction\n";
-printf STDERR "%d %.1f %d\n", $min_ht, $min_speed, $min_dir;
-printf STDERR "%d %.1f %d\n", $max_ht, $max_speed, $max_dir;
-#$min_speed *= .2;
-#$max_speed *= .2;
+
+$i = 0;
+for (0..$ct-1) {
+	$sca = $speed[$i]/$total *15;
+	printf IN "%.0f 0 %f %f %f %f\n", $degree[$i], $height[$i], $sca, $degree[$i], $speed[$i];
+	$i++;
+}
+
+
+
+printf STDERR "Height\tSpeed\tDirection\n";
+printf STDERR "%d\t%.1f\t%d\n", $min_ht, $min_speed, $min_dir;
+printf STDERR "%d\t%.1f\t%d\n", $max_ht, $max_speed, $max_dir;
+#$min_speed *= .5;
+#$max_speed *= .5;
 $max_dir += 5;
 $min_dir -= 5;
 #system "psrose $in -Y2i -R0/30/0/360/0/30 -G0 -M0.04i/0.12i/0.1i/255/0/0 -W1p/50 -Ba1g1:'Wind Speed (m/sec)':/g25:'':/:.'Wind Direction (wind blowing toward)': -S3 -C -V > $out";
-system "makecpt -Cpanoply -T0/$max_ht/5 -I > cpt";
-system "psxy $in -Y2i -R$min_dir/$max_dir/0/$max_speed -Sc.1c -SV -N -Jpa.2/.2 -Ba30g30/f1g5 -Gred -Ccpt -K -P > $out";
-system "psscale -D12/6/5/.2c -Ccpt -Li -B:'':/:'Wind Levels (kmasl)': -O >> $out";
-system "ps2raster $out -A -Tg";
+`gmt makecpt -Cpanoply -T0/$max_ht/5 -I > cpt`;
+`gmt psxy --FONT_ANNOT_PRIMARY=8p,2,0 --FONT_LABEL=8p,2,0 $in -R$min_dir/$max_dir/0/$max_speed -Sc0.1c -SV+e -N -Jpa0.16/0.16 -Bxa30g30 -Bya10f1g5+u"ms^-1" -W0.5p,150 -Ccpt -K -P > $out`;
+`gmt psscale --FONT_LABEL=8p,2,0 --FONT_ANNOT_PRIMARY=8p,1,0 -D-1.0/1.5/4/.2c -Ccpt -E -Baf+l"km" -O >> $out`;
+`gmt ps2raster $out -A -Tg`;
